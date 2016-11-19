97259253b411d89d017b5daa753c4159a7368d95
mqschwanda <mqschwanda@gmail.com>
2016-11-19 04:47:23 -0500

    updated file structure, login, and map inputs

--- server.js
+++ server.js
@@ -33,6 +33,23 @@ server.use(express.static(process.cwd() + '/public'));
 var routes = require('./controller/index.js');
 server.use('/', routes);
 
+// MYSQL CONFIGURATION
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'database_development'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  console.log('\n==== mySQL ====\nCONNECTED TO DB: '+connection.config.database+'\n        ON PORT: '+connection.config.port+'\n        WITH ID: '+connection.threadId);
+});
+
 // LISTENER
 server.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
